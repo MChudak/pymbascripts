c32c6c3279586483d7ac253540b3d9b2e3c455f5
Maciej Chudak <chudak.maciej@gmail.com>
2016-05-29 16:26:09 +0200

    pylint compliance

--- pymbaseriesgui.py
+++ pymbaseriesgui.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+"""
+    A prototype GUI for the pymbaseries script.
+"""
 import sys
 import numpy as np
 from PyQt5.QtCore import Qt
@@ -7,32 +10,31 @@ from PyQt5.QtGui import QImage, QPixmap
 # mainwindowui.py has to be generated from mainwindow.ui with pyuic5
 from mainwindowui import Ui_MainWindow
 
-def onResize(event):
-    if ui.checkBox.checkState():
-        immax = imagedata.max()
-        immin = imagedata.min()
-        imbytes = (255*(imagedata-immin)/(immax-immin)).astype(np.uint8)
+def onresize(_):
+    """ A slot called when the label is resized. """
+    if UI.checkBox.checkState():
+        immax = IMAGEDATA.max()
+        immin = IMAGEDATA.min()
+        imbytes = (255*(IMAGEDATA-immin)/(immax-immin)).astype(np.uint8)
     else:
-        imbytes = imagebytes
-    image = QImage(imbytes.flatten(), imagebytes.shape[1], imagebytes.shape[0], QImage.Format_Grayscale8)    
-    ui.label.setPixmap(QPixmap.fromImage(image.scaled(ui.label.size(), Qt.KeepAspectRatio)))
+        imbytes = IMAGEBYTES
+    image = QImage(imbytes.flatten(), IMAGEBYTES.shape[1], IMAGEBYTES.shape[0],
+                   QImage.Format_Grayscale8)
+    UI.label.setPixmap(QPixmap.fromImage(image.scaled(UI.label.size(), Qt.KeepAspectRatio)))
 
-app = QApplication(sys.argv)
-window = QMainWindow()
-ui = Ui_MainWindow()
-ui.setupUi(window)
+APP = QApplication(sys.argv)
+WINDOW = QMainWindow()
+UI = Ui_MainWindow()
+UI.setupUi(WINDOW)
 
-ui.label.resizeEvent = onResize
-ui.checkBox.stateChanged.connect(onResize)
+UI.label.resizeEvent = onresize
+UI.checkBox.stateChanged.connect(onresize)
 
 # some fake 'photo' data to process
-xx = np.arange(0, 300)
-yy = np.arange(0, 200)
-imagedata = np.array([[np.cos(x/25) + np.cos(y/25) for x in xx] for y in yy])
-imagebytes = (100 + 56*(0.5+imagedata/4)).astype(np.uint8)
+XX = np.arange(0, 300)
+YY = np.arange(0, 200)
+IMAGEDATA = np.array([[np.cos(x/25) + np.cos(y/25) for x in XX] for y in YY])
+IMAGEBYTES = (100 + 56*(0.5+IMAGEDATA/4)).astype(np.uint8)
 
-window.show()
-try:
-    sys.exit(app.exec_())
-except:
-    pass
+WINDOW.show()
+sys.exit(APP.exec_())
